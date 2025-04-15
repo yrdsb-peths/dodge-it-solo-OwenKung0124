c69f56e97fd8b6421af28251b4bc63893adfff88
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-04-15 13:56:26 -0400

    Add banana

---
+++ Banana.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Banana extends Actor
+{
+    /**
+     * Act - do whatever the Bananna wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(-3);
+    }
+}
