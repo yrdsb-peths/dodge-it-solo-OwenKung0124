65b5743091d9e616d4cf7efa066db6e2f016b641
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-04-15 14:11:30 -0400

    Reset banana

--- Banana.java
+++ Banana.java
@@ -3,12 +3,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Banana extends Actor
 {
-    /**
-     * Act - do whatever the Bananna wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public void act()
     {
-        move(-3);
+        move(-10);
+        
+        if(getX() <= 0) {
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana(){
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        }else {
+            setLocation(600, 300);
+        }
     }
 }
